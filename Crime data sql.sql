ce7fc6aafee658023a2280c8577ef20b2affae2d
yogeshwari1706 <132433451+yogeshwari1706@users.noreply.github.com>
2023-05-04 22:58:41 +0530

    Add files via upload

---
+++ Crime data sql.sql
@@ -0,0 +1,45 @@
+create database crime;
+
+create table crime.crime_data (
+case_number varchar(20) not null,
+crime_category varchar(50) not null,
+crime_description varchar(60),
+crime_location varchar(50) not null,
+arrest_status varchar (10) not null
+);
+
+SELECT * FROM crime.crime_data;
+
+select * from crime.crime_data where crime_description = "over $500";
+
+select count(distinct crime_description) from crime_data where crime_description = "over $500";
+
+select distinct crime_category from crime_data order by crime_category;
+
+select count(distinct crime_category) from crime_data order by crime_category;
+
+SELECT COUNT(*) AS "Number of Cases" , crime_category
+FROM crime_data
+GROUP BY crime_category
+ORDER BY "Number of Cases" DESC ;
+
+
+SELECT COUNT(*) AS "Number of Cases" , crime_category
+FROM crime_data
+GROUP BY crime_category
+ORDER BY "Number of Cases" DESC;
+
+SELECT * FROM (SELECT *,
+ROW_NUMBER() OVER (PARTITION BY case_number ORDER BY case_number) AS rn
+    FROM crime_data) t
+WHERE rn = 1;
+
+
+create table crime.crime_data_analysis (
+case_number varchar(20) not null,
+crime_category varchar(50) not null,
+crime_description varchar(60),
+crime_location varchar(50) not null,
+arrest_status varchar (10) not null,
+primary key (crime_description, crime_location)
+); 
